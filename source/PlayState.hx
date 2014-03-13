1c23e38d78be2dfed23fb77a0aefe103b2c28c56
Patrick McKenna <patrick.a.mckenna@gmail.com>
2014-03-13 19:43:55 -0400

    add player and flip all sprites to match what flixel is expecting

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,6 +1,7 @@
 package;
 
 import flixel.FlxG;
+import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.text.FlxText;
@@ -24,6 +25,9 @@ class PlayState extends FlxState
 	 * The FlxTilemap we're using
 	 */
 	private var _collisionMap:FlxTilemap;
+
+
+	private var _player:FlxSprite;
 	
 	/**
 	 * Function that is called up when to state is created to set it up. 
@@ -38,6 +42,16 @@ class PlayState extends FlxState
 		// Initializes the map using the generated string, the tile images, and the tile size
 		_collisionMap.loadMap(Assets.getText("assets/data/test_tilemap.txt"), "assets/images/wall1_tiles.png", TILE_WIDTH, TILE_HEIGHT, FlxTilemap.AUTO);
 		add(_collisionMap);
+
+		setupPlayer();
+	}
+
+	public function setupPlayer():Void {
+		_player = new FlxSprite(64, 220);
+		_player.loadGraphic("assets/images/player.png", true, true, 16);
+		_player.animation.add("idle", [0, 1], 2);
+		
+		add(_player);
 	}
 	
 	/**
@@ -54,6 +68,31 @@ class PlayState extends FlxState
 	 */
 	override public function update():Void
 	{
+		// Tilemaps can be collided just like any other FlxObject, and flixel
+		// automatically collides each individual tile with the object.
+		FlxG.collide(_player, _collisionMap);
+
+		updatePlayer();
+
 		super.update();
-	}	
+	}
+
+	public function updatePlayer():Void {
+		_player.animation.play("idle");
+
+		if (FlxG.keys.anyJustPressed(["LEFT", "A"])) {
+			_player.facing = FlxObject.LEFT;
+			_player.x -= 16;
+
+		} else if (FlxG.keys.anyJustPressed(["RIGHT", "D"])) {
+			_player.facing = FlxObject.RIGHT;
+			_player.x += 16;
+		}
+
+		if (FlxG.keys.anyJustPressed(["UP", "W"])) {
+			_player.y -= 16;
+		} else if (FlxG.keys.anyJustPressed(["DOWN", "S"])) {
+			_player.y += 16;
+		}
+	}
 }
